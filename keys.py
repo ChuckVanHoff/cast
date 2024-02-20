6657f7d84da621427b3c6a13abbfbc7e3eaebf84
Brian VanHoff <brian.vanhoff@gmail.com>
2024-02-20 08:09:58 -0500

    added api key for weatherapi

--- keys.py
+++ keys.py
@@ -2,4 +2,7 @@
 
 # OpenWeatherMaps
 # These can be the same or different
-test = 'd9ae6dd1af250d378109246f7ca8e1aa'
+owm_test = 'd9ae6dd1af250d378109246f7ca8e1aa'
+
+# WeatherAPI.com
+weatherapi_test = '112059454ee341ffa31205817241702'
